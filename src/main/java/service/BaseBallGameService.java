5f9a88da9f9c8dffb0633671101629b3cd8d0117
ojiwon <ojw970725@gmail.com>
2024-05-31 16:45:26 +0900

    feat : 1게임 play BaseBallGameService 서비스 구현

---
+++ src/main/java/service/BaseBallGameService.java
@@ -0,0 +1,25 @@
+package service;
+
+import domain.Balls;
+import domain.GameTotalResult;
+import factory.AnswerFactory;
+import view.AnswerInputView;
+
+public class BaseBallGameService {
+
+    public void play() {
+        Balls answer = AnswerFactory.generate();
+        GameTotalResult result = new GameTotalResult();
+        System.out.println(answer.getBalls());
+
+        while (!result.isEnd()) {
+            Balls userAnswer = AnswerInputView.inputUserAnswer();
+
+            result = answer.match(userAnswer);
+            System.out.println(result.getGameTotalResult());
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    }
+}
