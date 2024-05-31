cf9a922038b204980b8bab77a24738878cd74ddb
ojiwon <ojw970725@gmail.com>
2024-05-31 16:53:05 +0900

    refactor : 게임진행여부 enum으로 관리하도록 수정

--- src/main/java/view/GameProgressInputView.java
+++ src/main/java/view/GameProgressInputView.java
@@ -1,13 +1,17 @@
 package view;
 
+import domain.GAME_PROGRESS;
+
 import java.util.Scanner;
 
 public class GameProgressInputView {
 
-    public static int getInput() {
+    public static GAME_PROGRESS getInput() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+
+        int inputCode = scanner.nextInt();
+        return GAME_PROGRESS.get(inputCode);
     }
 }
